6567fbecdfdcd73329220e383bd746e189859387
DrEPL <dolnickenzanza@gmail.com>
2026-04-29 22:39:33 +0200

    feat: initialize custom app component with layout, SEO configuration, and analytics integration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -8,25 +8,25 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <DefaultSeo
-        title="Dr EPL | Plateforme de Solutions IA & Big Data"
-        description="Dr EPL Business, plateforme officielle spécialisée en Intelligence Artificielle et Big Data. Découvrez nos solutions, services et innovations technologiques."
+        title="Dr EPL | Ingénieur IA & Big Data"
+        description="Dr EPL, ingenieur IA & Big Data, plateforme spécialisé en Intelligence Artificielle et Big Data. Découvrez nos solutions, services et innovations technologiques."
         openGraph={{
           type: 'website',
           locale: 'fr_FR',
           url: 'https://drepl.cg/',
-          siteName: 'Dr EPL Business',
+          siteName: 'Dr EPL',
           images: [
             {
               url: 'https://drepl.cg/logo.png',
               width: 1200,
               height: 630,
-              alt: 'Dr EPL Business',
+              alt: 'Dolnick Prudhome ENZANZA Logo',
             },
             {
               url: 'https://drepl.cg/drepl.jpg',
               width: 800,
               height: 800,
-              alt: 'Dr EPL Business',
+              alt: 'Dolnick Prudhome ENZANZA',
             }
           ],
         }}
